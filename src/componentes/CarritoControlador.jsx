bd7cd6cbd626ac361bc31fee5e901175847b89cd
SebastianGarrido05 <se.garridom@duocuc.cl>
2025-11-03 18:39:33 -0300

    Añadido contador y botones de manejo del mismo en el carrito
    Se crea una ventana de alerta si quieres bajar de 1 a 0 el contador de algun producto
    
    Se modifica el css de los elementos del carrito
    
    Se añade precio unitario del producto por apartado

--- src/componentes/CarritoControlador.jsx
+++ src/componentes/CarritoControlador.jsx
@@ -30,8 +30,18 @@ export const CarritoControlador = ({ children }) => {
   const eliminarProducto = (id) => setCarrito(carrito.filter(p => p.id !== id));
   const limpiarCarrito = () => setCarrito([]);
 
+  const modificarCantidad = (id, cantidad) => {
+    setCarrito((prevCarrito) => {
+      return prevCarrito.map((p) =>
+        p.id === id ? { ...p, cantidad: cantidad } : p
+      ).filter((p) => p.cantidad > 0);     // Elimina el producto cuando llega a cero (TOMAR APUNTES EN DRIVE PARA NO OLVIDAR, IMPORTANTE)
+    });
+  };
+
+
   return (
-    <CarritoContext.Provider value={{ carrito, agregarProducto, eliminarProducto, limpiarCarrito }}>
+    <CarritoContext.Provider
+      value={{carrito, agregarProducto, eliminarProducto, limpiarCarrito, modificarCantidad}}>
       {children}
     </CarritoContext.Provider>
   );
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -3,9 +3,23 @@ import { CarritoContext } from "../componentes/CarritoControlador";
 import "../css/carrito.css";
 
 export default function Carrito() {
-  const { carrito, eliminarProducto, limpiarCarrito } = useContext(CarritoContext);
+  const { carrito, eliminarProducto, limpiarCarrito, modificarCantidad} = useContext(CarritoContext);
 
   const total = carrito.reduce((acc, p) => acc + p.precio * p.cantidad, 0);
+
+  const handleRestar = (producto) => {
+    if (producto.cantidad === 1) {
+      // Si llega a 0, mostramos alerta
+      const confirmar = window.confirm(`¿Deseas eliminar "Sensor de ${producto.nombre}" del carrito?`);
+      if (confirmar) eliminarProducto(producto.id);
+    } else {
+      modificarCantidad(producto.id, producto.cantidad - 1);
+    }
+  };
+
+  const handleSumar = (producto) => {
+    modificarCantidad(producto.id, producto.cantidad + 1);
+  };
 
   return (
     <div className="carrito-page">
@@ -19,8 +33,13 @@ export default function Carrito() {
               <li key={p.id}>
                 <img src={p.imagen} alt={p.nombre} />
                 <span>{p.nombre}</span>
-                <span>Cantidad: {p.cantidad}</span>
-                <span>Precio: ${p.precio * p.cantidad}</span>
+                <span>Precio Unitario: ${p.precio}</span>
+                <span>Precio * Cantidad: ${p.precio * p.cantidad}</span>
+                <div className="cantidad-control">
+                  <button onClick={() => handleRestar(p)}>-</button>
+                  <span>{p.cantidad}</span>
+                  <button onClick={() => handleSumar(p)}>+</button>
+                </div>
                 <button onClick={() => eliminarProducto(p.id)}>Eliminar</button>
               </li>
             ))}
